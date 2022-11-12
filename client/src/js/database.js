c858f0bd8b29ebd08fc8f96a247c8d15ac4363f6
Brayden <banjosquash@gmail.com>
2022-11-12 16:12:15 -0500

    added put and get requests from export functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-    console.log('PUT to the database');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.put({ id: 1, value: content });
+  const res = await req;
+
+  console.log('updated text entry saved to DB!', res);
+};
+
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('data received from the DB');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+
+  console.log(res.value);
+};
 
 initdb();
