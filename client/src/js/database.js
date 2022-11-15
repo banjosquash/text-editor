fcd52fb24b7b9bd2c205ece9ec4b6aa0b85e6464
Brayden <banjosquash@gmail.com>
2022-11-15 09:11:28 -0500

    added handlers for install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// --CHECK-- TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
 
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   console.log('updated text entry saved to DB!', res);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// --CHECK-- TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('data received from the DB');
 
